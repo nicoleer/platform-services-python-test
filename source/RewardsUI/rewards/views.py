9ed937e339aba2dd6fbe2701b575d8697b6e76b1
nicoleer <nicoleerkis+git@gmail.com>
2017-09-23 15:06:58 -0400

    Add Orders functionality

--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -1,9 +1,11 @@
 import logging
 import requests
 
+from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
 
+from .forms import OrderForm
 
 class RewardsView(TemplateView):
     template_name = 'index.html'
@@ -13,6 +15,9 @@ class RewardsView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
+
+        # Display form
+        context["order_form"] = OrderForm()
 
         rewards_response = requests.get("http://rewardsservice:7050/rewards")
         context['rewards_data'] = rewards_response.json()
@@ -25,3 +30,17 @@ class RewardsView(TemplateView):
             self.template_name,
             context
         )
+
+
+    def post(self, request, *args, **kwargs):
+        print('request=%r' % request)
+        if request.method == 'POST':
+            order_form = OrderForm(request.POST)
+            if order_form.is_valid():
+                response = requests.post("http://rewardsservice:7050/customer/set_rewards", 
+                              data = { "email" : order_form.cleaned_data['email'], 
+                                       "order_total" : order_form.cleaned_data['order_total'] 
+                                     })
+                print('response=%r' % response.text)
+
+                return HttpResponseRedirect('/rewards')
